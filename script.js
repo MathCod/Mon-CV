f3dcca87cfcc3b45a27ce6d6cd54f17b44d839c6
Mathias BERGER <mathias.codage@gmail.com>
2025-08-14 19:05:52 +0200

    cv github

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Bouton téléchargement PDF
 document.getElementById("downloadPDF").addEventListener("click", () => {
-    window.open("cv-mathias-berger.pdf", "_blank"); // Remplace par ton vrai lien ou chemin
+    window.open("https://github.com/MathCod/Mon-CV/cv-mathias-berger.pdf", "_blank"); // Remplace par ton vrai lien ou chemin
 });
 
 // Mode sombre
